188d4ef13a106eefe09d6e521bf00763c224e94b
nazeertsm <59368254+nazeertsm@users.noreply.github.com>
2022-04-17 15:42:09 +0530

    Add files via upload

---
+++ week1.day2/assignments/MissingElementInAnArray.java
@@ -0,0 +1,26 @@
+package week1.day2.assignments;
+import java.util.Arrays;
+
+public class MissingElementInAnArray {
+
+    public static void main(String[] args) {
+        // Here is the input
+        int[] arr = {1,2,3,4,7,6,8};
+
+        // Sort the array
+        Arrays.sort(arr);
+
+        // loop through the array (start i from arr[0] till the length of the array)
+        for (int i=0; i< arr.length; i++){
+
+        // check if the iterator variable is not equal to the array values respectively
+
+
+        // print the number
+            System.out.println(arr[i]);
+
+        // once printed break the iteration
+        }
+    }
+
+}
