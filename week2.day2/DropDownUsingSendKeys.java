bb1b3db86929ebc870bf182ebf2477535aedc586
nazeertsm <59368254+nazeertsm@users.noreply.github.com>
2022-04-28 16:19:30 +0530

    Add files via upload

---
+++ week2.day2/DropDownUsingSendKeys.java
@@ -0,0 +1,22 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DropDownUsingSendKeys {
+
+	public static void main(String[] args) {
+		WebDriverManager.edgedriver().setup();
+		EdgeDriver driver = new EdgeDriver();
+
+		System.out.println("TC1: Select country");
+		driver.get("https://www.opencart.com/index.php?route=account/register");
+		driver.findElement(By.id("input-country")).sendKeys("United States");
+		driver.findElement(By.id("input-country")).sendKeys("India");
+
+	}
+
+}
